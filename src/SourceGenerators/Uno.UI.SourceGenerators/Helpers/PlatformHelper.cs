1a95f562ff039822092438c3f511fd95f8ab1211
Steve Bilogan <steve.bilogan@gmail.com>
2021-10-13 23:02:04 -0400

    fix: Retarget Android assets with path names

--- src/SourceGenerators/Uno.UI.SourceGenerators/Helpers/PlatformHelper.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/Helpers/PlatformHelper.cs
@@ -1,5 +1,4 @@
-﻿
-#nullable enable
+﻿#nullable enable
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +9,13 @@ using Uno.Roslyn;
 
 #if NETFRAMEWORK
 using Uno.SourceGeneration;
+using Uno.UI;
+using Uno.UI.SourceGenerators;
+using Uno.UI.SourceGenerators.Helpers;
+using Uno.UI.SourceGenerators.XamlGenerator;
 #endif
 
-namespace Uno.UI.SourceGenerators.XamlGenerator
+namespace Uno.UI.SourceGenerators.Helpers
 {
 	public class PlatformHelper
 	{
@@ -38,5 +41,26 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 				&& !isWindowsRuntimeMetadataOutput
 				&& !isWindowsRuntimeApplicationOutput;
 		}
+
+		public static bool IsAndroid(GeneratorExecutionContext context)
+			=> context.GetMSBuildPropertyValue("AndroidApplication")?.Equals("true", StringComparison.OrdinalIgnoreCase) ?? false;
+
+		public static bool IsiOS(GeneratorExecutionContext context)
+			=> context.GetMSBuildPropertyValue("ProjectTypeGuidsProperty")?.Equals("{FEACFBD2-3405-455C-9665-78FE426C6842},{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}", StringComparison.OrdinalIgnoreCase) ?? false;
+
+		public static bool IsMacOs(GeneratorExecutionContext context)
+			=> context.GetMSBuildPropertyValue("ProjectTypeGuidsProperty")?.Equals("{A3F8F2AB-B479-4A4A-A458-A89E7DC349F1},{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}", StringComparison.OrdinalIgnoreCase) ?? false;
+
+		public static bool IsExe(GeneratorExecutionContext context)
+			=> context.GetMSBuildPropertyValue("OutputType")?.Equals("Exe", StringComparison.OrdinalIgnoreCase) ?? false;
+
+		public static bool IsUnoHead(GeneratorExecutionContext context)
+			=> context.GetMSBuildPropertyValue("IsUnoHead")?.Equals("true", StringComparison.OrdinalIgnoreCase) ?? false;
+
+		public static bool IsApplication(GeneratorExecutionContext context)
+			=> IsAndroid(context)
+				|| (IsiOS(context) && IsExe(context))
+				|| (IsMacOs(context) && IsExe(context))
+				|| IsUnoHead(context);
 	}
 }
